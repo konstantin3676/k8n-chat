cea631f1285cf5d1a3a22fba2a5b3f6e805f176b
Konstantin <93384228+konstantin3676@users.noreply.github.com>
2026-03-20 21:00:21 +0300

    Add main page

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+import { MainPage } from '../pages/MainPage';
+
+export const App = () => {
+  return (
+    <Box minHeight="100vh">
+      <MainPage />
+    </Box>
+  );
+};
